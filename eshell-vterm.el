1b762ec595760e3041a9df5fc0a2d3a8719bdc14
Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
2021-10-07 21:16:39 +0200

    Add package info

--- eshell-vterm.el
+++ eshell-vterm.el
@@ -1,5 +1,33 @@
 ;;; eshell-vterm.el --- Vterm for visual commands in eshell. -*- lexical-binding: t; -*-
+
+;; Author: Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
+;; Maintainer: Illia Ostapyshyn <ilya.ostapyshyn@gmail.com>
+;; Created: 2021-06-29
+;; Version: 0.1
+;; Package-Requires: ((emacs "27") (vterm "0.0.1"))
+;; Keywords: eshell, vterm, terminal, shell, visual
+;; URL: https://github.com/iostapyshyn/eshell-vterm
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
+
+;; This package provides a global minor mode allowing eshell to use
+;; vterm for visual commands.
+
 ;;; Code:
 
 (require 'vterm)
